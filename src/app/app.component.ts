78aabc94f2d7a2dec710a4a485b82f43252ee822
JDonadio <tanojaja@gmail.com>
2018-06-03 15:43:40 -0300

    set background depending on time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,23 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class WeatherApp {
   rootPage: any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform, 
+    statusBar: StatusBar, 
+    splashScreen: SplashScreen
+  ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  getTimeClass() {
+    let currentTime = (new Date()).getHours();
+    if (currentTime > 6 && currentTime <= 16) return 'day';
+    if (currentTime > 16 && currentTime <= 19) return 'afternoon';
+    if ((currentTime > 19 && currentTime <= 24) || (currentTime > 0 && currentTime <= 6)) return 'night';
   }
 }
